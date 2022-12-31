6be71f5636f77a7d6206d5a1c1317ee3a03bd1ef
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2022-12-31 08:57:30 -0500

    testing update

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,16 @@ import { expect, it } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange phase
+  const numbers = [1, 2];
+
+  // Act phase
+  const result = add(numbers);
+
+  // Assert phase
+  const expectedResult = numbers.reduce(
+    (preValue, curValue) => preValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
 });
